e384c4e886f58ef726074d00a2e4674815bfdfd5
SUNxuedian <sunxt16@qq.com>
2017-05-19 01:15:53 +0800

    实现显示高德地图

--- app/src/main/java/com/sun/recordway/MainActivity.java
+++ app/src/main/java/com/sun/recordway/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
 import com.yalantis.guillotine.animation.GuillotineAnimation;
 
 import butterknife.BindView;
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout root;
     @BindView(R.id.tv_title)
     TextView tv_title;
+    @BindView(R.id.map)
+    MapView  mMapView;
 
     private LinearLayout line_menu_mine;
     private LinearLayout line_menu_map;
@@ -53,6 +57,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         initView();
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mMapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        AMap aMap = null;
+
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
     }
 
     private void initView(){
@@ -114,6 +126,34 @@ public class MainActivity extends AppCompatActivity {
                 onClickMenuOfSetting();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
     }
 
     private void onClickMenuOfMine(){
